e88621d92b659ea0e9cb3922c864d583e09b210d
MMR Consultoria <maricelisrossi@gmail.com>
2025-09-07 21:29:01 -0300

    Update Operacional.py

--- pages/Operacional.py
+++ pages/Operacional.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 import traceback
 
-# st.set_page_config(page_title="Painel OS", page_icon="", layout="wide")
+st.set_page_config(page_title="Painel OS", page_icon="", layout="wide")
 
 # ========= Guarda de sessão =========
 if not st.session_state.get("acesso_liberado"):
